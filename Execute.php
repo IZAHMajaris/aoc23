f6e152cdd70cc31f65d366f6ad15b1820f585f98
d.wittig <d.wittig@axilaris.de>
2023-12-01 05:42:14 +0100

    AOC-Tag1
     -Aufbau Ordner noch einmal verändert

--- Execute.php
+++ Execute.php
@@ -5,18 +5,24 @@ class AOCClass
     public function importData()
     {
         $file = file_get_contents('./input', true);
-
         return explode(PHP_EOL, $file);
     }
 
-    public function execute() {
-        $test = $this->importData();
-        var_dump($test);
+    /* Helper Funktionen - Start */
+
+    /* Helper Funktionen - Ende */
+
+    public function Aufgabe01() {
+        $data = $this->importData();
+
+        return $data;
     }
 
-
+    public function output() {
+        var_dump($this->Aufgabe01());
+    }
 }
 
 $b = new AOCClass();
-$b->execute();
+$b->output();
 
